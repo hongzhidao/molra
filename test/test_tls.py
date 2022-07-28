5d75c9388f52fdeb177e7fd8183149acfb324877
Andrei Zeliankou <zelenkov@nginx.com>
2022-07-28 14:12:19 +0100

    Tests: added flags to search functions in proto.py.
    Also removed unnesessary re.compile() calls.
    
    (cherry picked from commit 69e690affe94eb37308d1eb551b34cd32323aae1)

--- test/test_tls.py
+++ test/test_tls.py
@@ -1,5 +1,4 @@
 import io
-import re
 import ssl
 import subprocess
 import time
@@ -640,9 +639,7 @@ basicConstraints = critical,CA:TRUE"""
         skip_alert(r'process .* %s.* exited on signal 9' % app_id)
 
         self.wait_for_record(
-            re.compile(
-                r' (?!' + app_id + r'#)(\d+)#\d+ "mirror" application started'
-            )
+            r' (?!' + app_id + r'#)(\d+)#\d+ "mirror" application started'
         )
 
         resp = self.post_ssl(
