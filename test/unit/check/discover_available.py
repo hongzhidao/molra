c8ccf043c8b191cdf1d0c6b669d75c5f9ff926d1
Zhidao HONG <hongzhidao@gmail.com>
2026-08-15 03:53:33 +0000

    Remove Node.js application support.

--- test/unit/check/discover_available.py
+++ test/unit/check/discover_available.py
@@ -5,7 +5,6 @@ from unit.check.chroot import check_chroot
 from unit.check.go import check_go
 from unit.check.isolation import check_isolation
 from unit.check.njs import check_njs
-from unit.check.node import check_node
 from unit.check.regex import check_regex
 from unit.check.tls import check_openssl
 from unit.log import Log
@@ -34,7 +33,6 @@ def discover_available(unit):
 
     option.available['modules']['go'] = check_go()
     option.available['modules']['njs'] = check_njs(output_version)
-    option.available['modules']['node'] = check_node()
     option.available['modules']['openssl'] = check_openssl(output_version)
     option.available['modules']['regex'] = check_regex(output_version)
 
--- test/unit/check/isolation.py
+++ test/unit/check/isolation.py
@@ -2,7 +2,6 @@ import json
 import os
 
 from unit.applications.lang.go import ApplicationGo
-from unit.applications.lang.node import ApplicationNode
 from unit.applications.lang.ruby import ApplicationRuby
 from unit.http import HTTP1
 from unit.option import option
@@ -73,22 +72,6 @@ def check_isolation():
                     "processes": {"spare": 0},
                     "working_directory": option.temp_dir + "/ruby/empty",
                     "script": option.temp_dir + "/ruby/empty/config.ru",
-                    "isolation": {"namespaces": {"credential": True}},
-                }
-            },
-        }
-
-    elif 'node' in available['modules']:
-        ApplicationNode().prepare_env('basic')
-
-        conf = {
-            "listeners": {"*:8080": {"pass": "applications/basic"}},
-            "applications": {
-                "basic": {
-                    "type": "external",
-                    "processes": {"spare": 0},
-                    "working_directory": option.temp_dir + "/node",
-                    "executable": "app.js",
                     "isolation": {"namespaces": {"credential": True}},
                 }
             },
